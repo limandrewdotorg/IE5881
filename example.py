b5d610ec67a8e14ffb49beed55a7273480db79e0
limandrewdotorg <69383852+limandrewdotorg@users.noreply.github.com>
2021-01-20 17:29:16 +0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,16 @@
+'''
+Author: Che Yuxin
+Date: 2021-01-05 20:24:49
+LastEditors: Che Yuxin
+LastEditTime: 2021-01-07 16:39:02
+Description: ...
+'''
+def add(a, b):
+   """This program adds two
+   numbers and return the result"""
+
+   result = a + b
+   return result
+
+c=add(1,2)
+print(c)
